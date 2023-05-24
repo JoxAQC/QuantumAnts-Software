94f86660f2a5dc8dca2c32dc809e43a6f7b566d7
unknown <max.saavedra@unmsm.edu.pe>
2023-05-24 16:34:27 -0500

    Clase Principal

--- Desarrollo/SGDS/Codificación/vista/App.py
+++ Desarrollo/SGDS/Codificación/vista/App.py
@@ -1,18 +1,6 @@
 from modelo.Hospital import Hospital
 from modelo.Donante import Donante
 from modelo.Credencial import Credencial
-<<<<<<< HEAD
-from modelo.Registro import Registro
-
-registrar = Registro(True)
-donante = Donante(1, "Juan Pérez", "date(1985, 5, 10)", "1234567890", "987654321", "Calle 123", "A+", "RH+")
-credencial = Credencial(1, "12/02/2024","12/02/2025", "Activo", "Donante", 
-                        "RogelioElCrag", "1234")
-registrar.insertar_credencial(credencial)
-hospital = Hospital("Maria Auxiliadora", "Las Perlas #524", "987654321", 
-                    "Activo", "No Creado", "No Creado", "No creado", credencial)
-registrar.agregarHospitalBD(hospital)
-=======
 from modelo.Condiciones import Condiciones
 from modelo.Beneficios import Beneficios
 from modelo.Horarios import Horarios
@@ -23,4 +11,3 @@ import json
 print("hola")
 #credencial = Credencial(1, "12/02/2024","12/02/2025", "Activo", "Donante", 
 #                       "RogelioElCrag", "1234")
->>>>>>> 66e0230f2f20bdfa0636dced2301eaa2ce498085
