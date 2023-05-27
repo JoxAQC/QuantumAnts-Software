7f4469d1ba6569aecced6f9eafb2e483ae9143c3
Geomar Fernandez Camacho <geomar.fernandez@unmsm.edu.pe>
2023-05-27 00:22:42 -0500

    Actualizar carpeta Codificación Clase Donante

--- Desarrollo/SGDS/Codificación/modelo/Sistema.py
+++ Desarrollo/SGDS/Codificación/modelo/Sistema.py
@@ -3,10 +3,12 @@ from Credenciales import Credencial
 from Condiciones import Condiciones
 from Beneficios import Beneficios
 from Horarios import Horarios
+from Donante import Donante
 import sqlite3 as sql
 import os
 import json
 import random
+
 
 class Sistema:
     def __init__(self, estado):
@@ -35,7 +37,7 @@ class Sistema:
         cursor = conn.cursor()
         instruction = "INSERT INTO Hospital VALUES (?, ?, ?, ?, ?)"
         cursor.execute(instruction, (hospital.get_idHospital(), hospital.get_nombreDeHospital(), hospital.get_direccion(), hospital.get_telefono(), hospital.get_estado()))
-            
+
         for condicion in hospital.get_condiciones():
             idCondicion = condicion.get_idCondicion()
             descripcion = condicion.get_descripcion()
@@ -56,7 +58,7 @@ class Sistema:
             hora = horario.get_hora()
             instruction = "INSERT INTO HorarioDeAtencion VALUES (?, ?, ?, ?)"
             cursor.execute(instruction, (idHorario, descripcion, hora, hospital.get_idHospital()))
-        
+
         conn.commit()
         conn.close()
 
@@ -79,40 +81,139 @@ class Sistema:
         id_credencial = random.randint(1000000, 9999999)
         return id_credencial
 
-def mainR1():
-         
-    #datos_licencias = {
-    #"licencias_vigentes": [
-    #    "123456",
-    #    "789012",
-    #    "345678",
-    #    "901234"
-    #]
-    #}
-    # Escribir datos en el archivo JSON
-    #with open("LicenciasVigentesFuncionamiento.json", "w") as archivo:
-    #    json.dump(datos_licencias, archivo)
+    ##########
+    def conectar_bd():  
+        # Obtener la ruta absoluta al archivo de base de datos
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(current_directory, "..", "serializar", "SGDS-VABD01.db")
 
-    idHospitalyCredencial = Sistema.generar_id_credencial()
-    credencial = Credencial(idHospitalyCredencial, "12/02/2024","12/02/2025", "Activo", "Donante", 
-                "RogelioElCrag", "1234")
-    horariolista = list()
-    horario1 = Horarios(1, "Horario 1", "08:00",idHospitalyCredencial)
-    horario2 = Horarios(2, "Horario 2", "12:00",idHospitalyCredencial)
-    horario3 = Horarios(3, "Horario 3", "16:00",idHospitalyCredencial)
-    horariolista.append(horario1)
-    horariolista.append(horario2)
-    horariolista.append(horario3)
-    condicionesLista = list()
-    condicion1 = Condiciones(1,"nose",idHospitalyCredencial)
-    condicionesLista.append(condicion1)
-    beneficiosLista = list()
-    beneficio1 = Beneficios(1,"nose",idHospitalyCredencial,1,1)
-    beneficiosLista.append(beneficio1)
-    hospital = Hospital(idHospitalyCredencial,"Maria Auxiliadora", "Las Perlas #524", "987654321", 
-                    "Activo", condicionesLista,beneficiosLista, horariolista, credencial)
-    numeroLicencia="123456"
-    Sistema.registrarHospital(hospital,credencial,numeroLicencia)
- 
-if __name__=="__main__":
-    mainR1()
+        # Establecer conexión con la base de datos
+        conn = sql.connect(db_path)
+        return conn
+
+    def registrar_donante(donante):
+        # Obtener la ruta absoluta del directorio actual
+        current_dir = os.path.abspath("")
+        # Construir la ruta absoluta del archivo de la base de datos
+        db_path = os.path.join(current_dir, "..", "serializar", "SGDS-VABD01.db")
+        # Establecer la conexión a la base de datos
+        conn = sql.connect(db_path)
+        cursor = conn.cursor()
+
+        try:
+            instruction = "INSERT INTO Donante VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(
+                instruction,
+                (
+                    donante.get_idDonante(),
+                    donante.get_nombre(),
+                    donante.get_fechaNacimiento(),
+                    donante.get_dni(),
+                    donante.get_telefono(),
+                    donante.get_direccion(),
+                    donante.get_beneficioActivo(),
+                    donante.get_grupoSanguineo(),
+                    donante.get_RH(),
+                    donante.get_ultimaDonacion(),
+                    donante.get_idHospitalUltimo(),
+                ),
+            )
+
+            conn.commit()
+            print("Donante registrado exitosamente.")
+        except sql.Error as e:
+            print("Error al registrar el donante:", str(e))
+
+        conn.close()
+
+
+    def buscar_donantes(self, grupo_sanguineo, ubicacion):
+        conn = self.conectar_bd()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "SELECT * FROM Donante WHERE grupo_sanguineo = ? AND direccion LIKE ?",
+                (grupo_sanguineo, f"%{ubicacion}%"),
+            )
+
+            donantes = cursor.fetchall()
+
+            return donantes
+        except sql.Error as e:
+            print("Error al buscar donantes:", str(e))
+
+        conn.close()
+        return []
+
+    def verCitas(self):
+        conn = self.conectar_bd()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("SELECT * FROM citas WHERE idDonante = ?", (self.idDonante,))
+            citas = cursor.fetchall()
+
+            if len(citas) > 0:
+                print("Citas:")
+                for cita in citas:
+                    print(f"Fecha: {cita[1]}, Hospital: {cita[2]}")
+            else:
+                    print("No hay citas programadas.")
+        except sql.Error as e:
+            print("Error al obtener las citas:", str(e))
+
+        conn.close()
+
+    def verBeneficiosObtenidos(self):
+        conn = self.conectar_bd()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "SELECT * FROM beneficios WHERE idDonante = ?", (self.idDonante,)
+            )
+            beneficios = cursor.fetchall()
+
+            if len(beneficios) > 0:
+                print("Beneficios obtenidos:")
+                for beneficio in beneficios:
+                    print(f"Fecha: {beneficio[1]}, Descripción: {beneficio[2]}")
+            else:
+                print("No se han obtenido beneficios.")
+        except sql.Error as e:
+            print("Error al obtener los beneficios:", str(e))
+
+        conn.close()
+
+
+    def cambiarDatos(self, nombre, fechaNacimiento, dni, telefono, direccion):
+        self.nombre = nombre
+        self.fechaNacimiento = fechaNacimiento
+        self.dni = dni
+        self.telefono = telefono
+        self.direccion = direccion
+
+        conn = self.conectar_bd()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "UPDATE Donante SET nombre = ?, fecha_nacimiento = ?, dni = ?, telefono = ?, "
+                "direccion = ? WHERE idDonante = ?",
+                (
+                    self.nombre,
+                    self.fechaNacimiento,
+                    self.dni,
+                    self.telefono,
+                    self.direccion,
+                    self.idDonante,
+                ),
+            )
+
+            conn.commit()
+            print("Datos actualizados exitosamente.")
+        except sql.Error as e:
+            print("Error al actualizar los datos:", str(e))
+
+        conn.close()
--- Desarrollo/SGDS/Codificación/vista/App.py
+++ Desarrollo/SGDS/Codificación/vista/App.py
@@ -1,1 +1,14 @@
+import sqlite3 as sql
+from Donante import Donante
+from Sistema import Sistema
+
+# Crear un objeto Donante
+donante = Donante("123", "Juan Pérez", "1990-01-01", "12345678", "987654321", "Calle Principal 123", True, "A+", "+", "2023-05-20", "H001")
+
+# Crear un objeto Sistema
+sistema = Sistema()
+
+# Registrar el donante en la base de datos utilizando el método de la clase Sistema
+sistema.registrar_donante(donante)
+
 
