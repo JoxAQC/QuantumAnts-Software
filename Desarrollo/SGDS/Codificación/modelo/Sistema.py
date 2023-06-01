2837d36af945b18443d9740bf972c8f2ac113fbb
Geomar Fernandez Camacho <geomar.fernandez@unmsm.edu.pe>
2023-05-31 23:47:00 -0500

    Modificado Donante, Sistema y App

--- Desarrollo/SGDS/Codificación/modelo/Sistema.py
+++ Desarrollo/SGDS/Codificación/modelo/Sistema.py
@@ -100,6 +100,145 @@ class Sistema:
         # Genera un número entero aleatorio de 7 dígitos
         id_credencial = random.randint(1000000, 9999999)
         return id_credencial
+    
+     ##########
+    def conectar_bd():  
+        # Obtener la ruta absoluta al archivo de base de datos
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        db_path = os.path.join(current_directory, "..", "serializar", "SGDS-VABD01.db")
+
+        # Establecer conexión con la base de datos
+        conn = sql.connect(db_path)
+        return conn
+
+    def registrar_donante(donante):
+        # Obtener la ruta absoluta del directorio actual
+        current_dir = os.path.abspath("")
+        # Construir la ruta absoluta del archivo de la base de datos
+        db_path = os.path.join(current_dir, "..", "serializar", "SGDS-VABD01.db")
+        # Establecer la conexión a la base de datos
+        conn = sql.connect(db_path)
+        cursor = conn.cursor()
+
+        try:
+            instruction = "INSERT INTO Donante VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            cursor.execute(
+                instruction,
+                (
+                    donante.get_idDonante(),
+                    donante.get_nombre(),
+                    donante.get_fechaNacimiento(),
+                    donante.get_dni(),
+                    donante.get_telefono(),
+                    donante.get_direccion(),
+                    donante.get_beneficioActivo(),
+                    donante.get_grupoSanguineo(),
+                    donante.get_RH(),
+                    donante.get_ultimaDonacion(),
+                    donante.get_idHospitalUltimo(),
+                ),
+            )
+
+            conn.commit()
+            print("Donante registrado exitosamente.")
+        except sql.Error as e:
+            print("Error al registrar el donante:", str(e))
+
+        conn.close()
+
+
+def buscar_donantes(donante, grupo_sanguineo, ubicacion):
+    conn = donante.conectar_bd()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            "SELECT * FROM Donante WHERE grupo_sanguineo = ? AND direccion LIKE ?",
+            (grupo_sanguineo, f"%{ubicacion}%"),
+        )
+
+        donantes = cursor.fetchall()
+
+        return donantes
+    except sql.Error as e:
+        print("Error al buscar donantes:", str(e))
+
+    conn.close()
+    return []
+
+
+def verCitas(donante):
+    conn = donante.conectar_bd()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM citas WHERE idDonante = ?", (donante.idDonante,))
+        citas = cursor.fetchall()
+
+        if len(citas) > 0:
+            print("Citas:")
+            for cita in citas:
+                print(f"Fecha: {cita[1]}, Hospital: {cita[2]}")
+        else:
+            print("No hay citas programadas.")
+    except sql.Error as e:
+        print("Error al obtener las citas:", str(e))
+
+    conn.close()
+
+
+def verBeneficiosObtenidos(donante):
+    conn = donante.conectar_bd()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            "SELECT * FROM beneficios WHERE idDonante = ?", (donante.idDonante,)
+        )
+        beneficios = cursor.fetchall()
+
+        if len(beneficios) > 0:
+            print("Beneficios obtenidos:")
+            for beneficio in beneficios:
+                print(f"Fecha: {beneficio[1]}, Descripción: {beneficio[2]}")
+        else:
+            print("No se han obtenido beneficios.")
+    except sql.Error as e:
+        print("Error al obtener los beneficios:", str(e))
+
+    conn.close()
+
+
+def cambiarDatos(donante, nombre, fechaNacimiento, dni, telefono, direccion):
+    donante.nombre = nombre
+    donante.fechaNacimiento = fechaNacimiento
+    donante.dni = dni
+    donante.telefono = telefono
+    donante.direccion = direccion
+
+    conn = donante.conectar_bd()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            "UPDATE Donante SET nombre = ?, fecha_nacimiento = ?, dni = ?, telefono = ?, "
+            "direccion = ? WHERE idDonante = ?",
+            (
+                donante.nombre,
+                donante.fechaNacimiento,
+                donante.dni,
+                donante.telefono,
+                donante.direccion,
+                donante.idDonante,
+            ),
+        )
+
+        conn.commit()
+        print("Datos actualizados exitosamente.")
+    except sql.Error as e:
+        print("Error al actualizar los datos:", str(e))
+
+    conn.close()
 
     def confirmarCita(cita):
         current_dir = os.path.abspath("")
